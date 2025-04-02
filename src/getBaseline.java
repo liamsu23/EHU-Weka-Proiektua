dde30fbaf9dfa6dd634518309a55190df31da810
mbriones002 <83336945+mbriones002@users.noreply.github.com>
2025-04-02 14:54:03 +0200

    Update getBaseline.java

--- src/getBaseline.java
+++ src/getBaseline.java
@@ -4,19 +4,23 @@ import weka.classifiers.functions.SimpleLogistic;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 
 public class getBaseline {
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
-            System.out.println("Uso: java BaselineRegression <train.arff> <dev.arff>");
+        if (args.length != 3) {
+            System.out.println("Uso: java BaselineRegression <train.arff> <dev.arff> <ebaluazioLR.txt>");
             return;
         }
 
         try {
             String trainPath = args[0];
-            String devPath = args[1];
+            String devPath= args[1];
+            String outEbaluazioakPath = args[2];
 
             // Cargar datasets
             ConverterUtils.DataSource sourceTrain = new ConverterUtils.DataSource(trainPath);
@@ -44,6 +48,7 @@ public class getBaseline {
             System.out.println("Evaluando modelo...");
             eval.evaluateModel(model, dataDev);
 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outEbaluazioakPath));
 
             // 5. Imprimir resultados de la evaluación
             System.out.println("=== Evaluación del Modelo con Dev ===");
@@ -53,9 +58,21 @@ public class getBaseline {
             System.out.println("=== Medidas de Precisión ===");
             System.out.println(eval.toClassDetailsString());
 
+            writer.write("=== Dev modeloaren ebaluazioa ===");
+            writer.write(eval.toSummaryString());
+            writer.write("=== Nahasmen matrizea ===");
+            writer.write(eval.toMatrixString());
+            writer.write("=== Precision ===");
+            writer.write(eval.toClassDetailsString());
+
             // Imprimir métricas de regresión
             System.out.println("Error absoluto medio (MAE): " + eval.meanAbsoluteError());
             System.out.println("Raíz del error cuadrático medio (RMSE): " + eval.rootMeanSquaredError());
+
+            writer.write("Mean Absolute Error (MAE): " + eval.meanAbsoluteError());
+            writer.write("Root Mean Squared Error (RMSE): " + eval.rootMeanSquaredError());
+            writer.close();
+
 
         } catch (Exception e) {
             e.printStackTrace();
